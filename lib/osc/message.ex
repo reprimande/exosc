66a8523e2b4c2b7addadb8f8f68a1ba1ed6520a4
Naoki Nomoto <happygrind@gmail.com>
2015-12-25 13:43:42 +0900

    add double / blob

--- lib/osc/message.ex
+++ lib/osc/message.ex
@@ -4,9 +4,10 @@ defmodule OSC.Message do
   end
 
   def parse_args(args) when is_list(args) do
-    { tags, values } = args
-    |> Enum.map(fn (x) -> parse_value(x) end)
-    |> Enum.unzip
+    { tags, values } =
+      args
+      |> Enum.map(fn (x) -> parse_value(x) end)
+      |> Enum.unzip
 
     t = [",", tags, <<0>>] |> List.flatten |> Enum.join |> padding
     v = values |> Enum.join
@@ -15,18 +16,34 @@ defmodule OSC.Message do
 
   def parse_args(args), do: parse_value(args)
 
-  def parse_value(value) when is_integer(value) do
+  def parse_value(value) when is_integer(value), do: parse_value({:int, value})
+
+  def parse_value(value) when is_float(value), do: parse_value({:float, value})
+
+  def parse_value(value) when is_binary(value), do: parse_value({:string, value})
+
+  def parse_value({:int, value}) do
     { "i", <<value :: big-signed-integer-size(32)>> }
   end
 
-  def parse_value(value) when is_float(value) do
+  def parse_value({:float, value}) do
     { "f", <<value :: big-signed-float-size(32)>> }
   end
 
-  def parse_value(value) when is_binary(value) do
+  def parse_value({:string, value}) do
     { "s", padding(value <> <<0>>) }
   end
 
+  def parse_value({:double, value}) do
+    { "d", <<value :: big-signed-float-size(64)>> }
+  end
+
+  def parse_value({:blob, value}) do
+    size = <<byte_size(value) :: big-signed-integer-size(32)>>
+    { "b", padding(size <> value <> <<0>>) }
+  end
+
   def padding(buf) when rem(byte_size(buf), 4) == 0, do: buf
+
   def padding(buf), do: buf <> <<0>> |> padding
 end
